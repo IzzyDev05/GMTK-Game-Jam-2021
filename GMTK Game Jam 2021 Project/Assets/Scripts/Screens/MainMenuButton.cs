19dc432dbc534004d4b7870fb0b65b3da8a4fee6
IzzyDev <66153582+IzzyDev05@users.noreply.github.com>
2021-06-12 19:42:05 +0530

    How2Play menu done

---
+++ GMTK Game Jam 2021 Project/Assets/Scripts/Screens/MainMenuButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour
+{
+    public void ToMainMenu() {
+        SceneManager.LoadScene(0);
+    }
+}
