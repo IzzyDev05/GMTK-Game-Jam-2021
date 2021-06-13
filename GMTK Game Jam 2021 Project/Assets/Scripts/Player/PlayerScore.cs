3f4704b1282c019e7f654d12360726dc91ada4e2
IzzyDev <66153582+IzzyDev05@users.noreply.github.com>
2021-06-13 09:14:37 +0530

    Added heart system

--- GMTK Game Jam 2021 Project/Assets/Scripts/Player/PlayerScore.cs
+++ GMTK Game Jam 2021 Project/Assets/Scripts/Player/PlayerScore.cs
@@ -8,8 +8,10 @@ public class PlayerScore : MonoBehaviour
     public static int playerScore = 0;
 
     [SerializeField] TMP_Text scoreText;
+    [SerializeField] TMP_Text scoreStat;
 
     private void Update() {
         scoreText.text = playerScore.ToString();
+        scoreStat.text = playerScore.ToString();
     }
 }
---
+++ GMTK Game Jam 2021 Project/Assets/Scripts/UI/HeartManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] filledHearts;
+    [SerializeField] GameObject[] emptyHearts;
+
+    private Player player;
+    private int playerHealth;
+
+    private void Start() {
+        player = FindObjectOfType<Player>();
+    }
+
+    private void Update() {
+        playerHealth = player.health;
+
+        UpdateHearts();
+    }
+
+    private void UpdateHearts() {
+        if (playerHealth == 5) {
+            foreach (GameObject heart in filledHearts) {
+                heart.SetActive(true);
+            }
+            foreach (GameObject heart in emptyHearts) {
+                heart.SetActive(false);
+            }
+        }
+        else if (playerHealth == 4) {
+            filledHearts[0].SetActive(false);
+            emptyHearts[0].SetActive(true);
+        }
+        else if (playerHealth == 3) {
+            filledHearts[1].SetActive(false);
+            emptyHearts[1].SetActive(true);
+        }
+        else if (playerHealth == 2) {
+            filledHearts[2].SetActive(false);
+            emptyHearts[2].SetActive(true);
+        }
+        else if (playerHealth == 1) {
+            filledHearts[3].SetActive(false);
+            emptyHearts[3].SetActive(true);
+        }
+        else {
+            foreach (GameObject heart in filledHearts) {
+                heart.SetActive(false);
+            }
+            foreach (GameObject heart in emptyHearts) {
+                heart.SetActive(true);
+            }
+        }
+    }
+}
